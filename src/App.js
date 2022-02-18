4308adabcbfc071504f7418dbaa9c46ccb0166c4
thesergioliveira <thesergioliveira@gmail.com>
2022-02-18 01:43:37 +0100

    adding scss basic file structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import './scss/style.scss';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Bingo</header>
+      <header className="header title">Bingo, this is rendering live!</header>
     </div>
   );
 }
